8a03bbcb1bf2e45ae54e7d11db39a4172c846a04
Diego Jonguitud G - Macbook <djonguitud95@gmail.com>
2022-10-05 14:06:45 -0500

    chore: apply stash changes style, code, markup

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,32 +1,41 @@
 'use strict';
 /* Global variables */
+
+//Section Start variables
 const elBtn = document.getElementById('btn-start');
 let elWelcome = document.querySelector('.welcome__wraper');
-let elQuestion = document.querySelector('.questions_wraper');
 
-//Timer variables
+//Section Questions variables
+let elQuestion = document.querySelector('.questions__wraper');
+
+//Feat Timer variables
 let elTimer = document.getElementById('timer');
-let seconds = 13;
-let minute = 10;
+let seconds = 60;
+let minute = 11;
 
 elBtn.addEventListener('click', startTest);
 
 //Add question list
 
-/* Function start test */
+/* Flow functions*/
+
+//Start test
 function startTest() {
 	//Disappear welcome card
 	elWelcome.className = 'hidden__wraper';
 	questionbox();
 }
 
-/* Function appear question box */
+/*Appear question box */
 function questionbox() {
 	//Remove and set the class for displaying box question
 	elQuestion.classList.remove('hidden__wraper');
+	elQuestion.classList.remove('questions__wraper');
 	elQuestion.classList.add('main__wraper');
 	timer;
 }
+
+/* Features functions */
 
 //Set timer set
 let timer = setInterval(function () {
@@ -48,7 +57,7 @@ let timer = setInterval(function () {
 		elTimer.textContent = `0${minute}:0${seconds}`;
 	} else if (seconds === 0) {
 		minute--;
-		seconds = 13;
+		seconds = 59;
 		/* elTimer.textContent = `0${minute}:${seconds}`; */
 	}
 }, 1000);
