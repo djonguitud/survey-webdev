362ed2cf7fcbb4f06635ebf1d8de6d88e3716f3b
Diego Jonguitud G - Macbook <djonguitud95@gmail.com>
2022-10-10 13:50:36 -0500

    perf: merge second-quiz-option branch for final prototype and performance

---
+++ assests/scripts/questions.js
@@ -0,0 +1,120 @@
+'use strict';
+
+//! Add question array
+let questions = [
+	{
+		q1: '¿Cuál es la manera recomendada de incrustar un archivo Javascript en el HTML5?',
+		answer1: {
+			answer: 'Al último del elemento body',
+			correct: true,
+		},
+		answer2: {
+			answer: 'En el elemento head del HTML5',
+			correct: false,
+		},
+		answer3: {
+			answer: 'Dejarlo en la carpeta de scripts',
+			correct: false,
+		},
+		answer4: {
+			answer: 'Ponerlo en cada línea de código HTML5',
+			correct: false,
+		},
+		answer5: {
+			answer: 'En el elemento Footer del HTML5',
+			correct: false,
+		},
+	},
+	{
+		q2: '¿Qué pasa si cambias los valores de una Array si está declarado con const?',
+		answer1: {
+			answer: 'Se borra',
+			correct: false,
+		},
+		answer2: {
+			answer: 'Marcará un error.',
+			correct: false,
+		},
+		answer3: {
+			answer: 'Se convierte en declaración var',
+			correct: false,
+		},
+		answer4: {
+			answer: 'No existe la declaración const',
+			correct: false,
+		},
+		answer5: {
+			answer: 'No marcará error, sigue el curso del código',
+			correct: true,
+		},
+	},
+	{
+		q3: '¿Qué significa una expresión?',
+		answer1: {
+			answer: 'Crear una variable.',
+			correct: false,
+		},
+		answer2: {
+			answer: 'Una operación que genera un producto.',
+			correct: true,
+		},
+		answer3: {
+			answer: 'El producto de una función.',
+			correct: false,
+		},
+		answer4: {
+			answer: 'Cuando nombras a una variable.',
+			correct: false,
+		},
+		answer5: {
+			answer: 'Ni siquiera es un término de Javascript.',
+			correct: false,
+		},
+	},
+	{
+		q4: '¿Qué significa shadowing?',
+		answer1: {
+			answer: 'Esconder algún elemento HTML5 usando código Javascript',
+			correct: false,
+		},
+		answer2: {
+			answer: 'Declarar una variable con el mismo nombre que otra.',
+			correct: false,
+		},
+		answer3: {
+			answer: 'No tengo idea',
+			correct: false,
+		},
+		answer4: {
+			answer: 'Declarar una variable local con el mismo nombre que una global.',
+			correct: true,
+		},
+		answer5: {
+			answer: 'Agregar un estilo de sombra a algún elemento.',
+			correct: false,
+		},
+	},
+	{
+		q5: '¿Cuántas opciones de declaraciones lógicas existen en Javascript?',
+		answer1: {
+			answer: 'Una',
+			correct: false,
+		},
+		answer2: {
+			answer: 'Dos',
+			correct: false,
+		},
+		answer3: {
+			answer: 'Varias opciones',
+			correct: false,
+		},
+		answer4: {
+			answer: 'Tres',
+			correct: true,
+		},
+		answer5: {
+			answer: 'No tengo idea',
+			correct: false,
+		},
+	},
+];
--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,259 +1,187 @@
 'use strict';
 
-/*  Global variables */
-//! Section Start variables
-const elBtn = document.querySelector('.btn');
-let elWelcome = document.querySelector('#main');
+/*=============================================
+=            VARIABLES SECTION            =
+=============================================*/
+//! Container Var
+let container = document.querySelector('.container');
 
-//! Section Questions variables
-let elQuestionWraper = document.querySelector('#question__wraper');
+//! Welcome elements Var
+let elWelcomeSctn = document.querySelector('#welcome');
+let startBtn = document.querySelector('.btn');
+
+//! Question Cards Var
+let elQuestionSctn = document.querySelector('#question__wraper');
 let elAnswersWraper = document.querySelector('.answers__wraper');
-let elAnswers = document.getElementById('click-dgtn');
-let elQuestionDec = document.getElementById('question');
-let elAnswersList = document.getElementsByTagName('li');
 
-//! Feat Timer variables
+//! Last stage Var
+let goodbye = document.querySelector('#goodbye');
+
+//!Object Questions Var
+let index = 0;
+
+//! Score Var
+let score = 0;
+
+//! Feat Timer Var
 let elTimer = document.getElementById('timer');
-let seconds = 1;
-let minute = 10;
+let seconds = 61;
 
-//! Add question array
-let questions = [
-	{
-		q1: 'Si hablamos de HTML5. ¿Cuál de estos NO es un elemento semántico?',
-		answer1: {
-			answer: 'Div',
-			correct: true,
-		},
-		answer2: {
-			answer: 'Section',
-			correct: false,
-		},
-		answer3: {
-			answer: 'Address',
-			correct: false,
-		},
-		answer4: {
-			answer: 'Main',
-			correct: false,
-		},
-		answer5: {
-			answer: 'Footer',
-			correct: false,
-		},
-	},
-	{
-		q2: 'Si hablamos de CSS3 y en tema de posición. ¿Cuál es el contexto de un elemento posicionado absoluto?',
-		answer1: {
-			answer: 'El Viewport.',
-			correct: false,
-		},
-		answer2: {
-			answer: 'Su elemento padre.',
-			correct: false,
-		},
-		answer3: {
-			answer: 'El Body.',
-			correct: false,
-		},
-		answer4: {
-			answer: 'Su elemento hermano.',
-			correct: false,
-		},
-		answer5: {
-			answer: 'Su elemento padre con posición relative.',
-			correct: true,
-		},
-	},
-	{
-		q3: 'Si hablamos de Javascript. ¿Qué significa una expresión?',
-		answer1: {
-			answer: 'Crear una variable.',
-			correct: false,
-		},
-		answer2: {
-			answer: 'Una operación que genera un producto.',
-			correct: true,
-		},
-		answer3: {
-			answer: 'El producto de una función.',
-			correct: false,
-		},
-		answer4: {
-			answer: 'Cuando nombras a una variable.',
-			correct: false,
-		},
-		answer5: {
-			answer: 'Ni siquiera es un término de Javascript.',
-			correct: false,
-		},
-	},
-	{
-		q4: 'Si hablamos de Javascript. ¿Qué significa shadowing?',
-		answer1: {
-			answer: 'Esconder algún elemento HTML5 usando código Javascript',
-			correct: false,
-		},
-		answer2: {
-			answer: 'Declarar una variable con el mismo nombre que otra.',
-			correct: false,
-		},
-		answer3: {
-			answer: 'No tengo idea',
-			correct: false,
-		},
-		answer4: {
-			answer: 'Declarar una variable local con el mismo nombre que una global.',
-			correct: true,
-		},
-		answer5: {
-			answer: 'Agregar un estilo de sombra a algún elemento.',
-			correct: false,
-		},
-	},
-	{
-		q5: 'Si hablamos de CSS. ¿Que selector tiene mayor relevancia o especificidad sobre todos?',
-		answer1: {
-			answer: 'Class',
-			correct: false,
-		},
-		answer2: {
-			answer: 'Id',
-			correct: false,
-		},
-		answer3: {
-			answer: 'Div',
-			correct: false,
-		},
-		answer4: {
-			answer: '!important',
-			correct: true,
-		},
-		answer5: {
-			answer: 'Un style en línea',
-			correct: false,
-		},
-	},
-];
+//! Form Var
+let elScoreFnl = document.querySelector('#score');
+let elInicials = document.querySelector('#iniciales');
+let subBtn = document.querySelector('#subBtn');
 
-/* Events listener  */
+/*=====  End of Section comment block  ======*/
 
-//! Start game
-elBtn.addEventListener('click', startTest);
-
-//! Delegation event to select answers list
-
-/* elAnswers.addEventListener('click', function (e) {
-	console.log(e.target.parentElement);
-}); */
-
-/* Flow functions */
-
-//! Start test
-function startTest() {
-	//Disappear welcome card
-	elWelcome.classList.remove('main__wraper');
-	elWelcome.className = 'hidden__wraper';
-	questionbox();
+/*=============================================
+=            FLOW DOM SECTION          =
+=============================================*/
+//! Add questions card
+function addQstnCard() {
+	qstnMarkUp();
+	updateQstn(index);
 }
 
-//! Appear question box
-function questionbox() {
-	//Remove and set the class for displaying box question
-	elQuestionWraper.classList.remove('hidden__wraper');
-	elQuestionWraper.classList.add('main__wraper');
-	questionSet(4);
-	timer;
+//! Add question markup
+function qstnMarkUp() {
+	elQuestionSctn.classList.remove('hidden__wraper');
+	elQuestionSctn.classList.add('main__wraper');
 }
 
-//! Set the group question & answers
-function questionSet(index) {
-	switch (index) {
+//!Remove welcome card
+function startGame() {
+	elWelcomeSctn.classList.add('hidden__wraper');
+	elWelcomeSctn.classList.remove('main__wraper');
+	addQstnCard();
+	setInterval(function () {
+		seconds--;
+		elTimer.textContent = `${seconds}`;
+		seconds === 0 ? quitGame() : false;
+	}, 1000);
+}
+
+//! Add question box
+function updateQstn(i) {
+	switch (i) {
 		case 0:
-			elAnswersWraper.innerHTML = `<p class="question-sntc" id="question">${questions[index]['q1']}</p>
+			elAnswersWraper.innerHTML = `<p id="question">${questions[index]['q1']}</p>
 			<ol class="answers__list__wraper" id="click-dgtn">
 				<li class="answer-item" data-iscorrect="${questions[index]['answer1']['correct']}">${questions[index]['answer1']['answer']}</li>
-				<li class="answer-item data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
-				<li class="answer-item data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
-				<li class="answer-item data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
-				<li class="answer-item data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
+				<li class="answer-item" data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
+				<li class="answer-item" data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
+				<li class="answer-item" data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
+				<li class="answer-item" data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
 			</ol>`;
 			break;
 		case 1:
-			elAnswersWraper.innerHTML = `<p class="question-sntc" id="question">${questions[index]['q2']}</p>
+			elAnswersWraper.innerHTML = `<p id="question">${questions[index]['q2']}</p>
 				<ol class="answers__list__wraper" id="click-dgtn">
 					<li class="answer-item" data-iscorrect="${questions[index]['answer1']['correct']}">${questions[index]['answer1']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
+					<li class="answer-item"data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
 				</ol>`;
 			break;
 		case 2:
-			elAnswersWraper.innerHTML = `<p class="question-sntc" id="question">${questions[index]['q3']}</p>
+			elAnswersWraper.innerHTML = `<p id="question">${questions[index]['q3']}</p>
 				<ol class="answers__list__wraper" id="click-dgtn">
 					<li class="answer-item" data-iscorrect="${questions[index]['answer1']['correct']}">${questions[index]['answer1']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
 				</ol>`;
 			break;
 		case 3:
-			elAnswersWraper.innerHTML = `<p class="question-sntc" id="question">${questions[index]['q4']}</p>
+			elAnswersWraper.innerHTML = `<p id="question">${questions[index]['q4']}</p>
 				<ol class="answers__list__wraper" id="click-dgtn">
 					<li class="answer-item" data-iscorrect="${questions[index]['answer1']['correct']}">${questions[index]['answer1']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
 				</ol>`;
 			break;
 		case 4:
-			elAnswersWraper.innerHTML = `<p class="question-sntc" id="question">${questions[index]['q5']}</p>
+			elAnswersWraper.innerHTML = `<p id="question">${questions[index]['q5']}</p>
 				<ol class="answers__list__wraper" id="click-dgtn">
 					<li class="answer-item" data-iscorrect="${questions[index]['answer1']['correct']}">${questions[index]['answer1']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
-					<li class="answer-item data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer2']['correct']}">${questions[index]['answer2']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer3']['correct']}">${questions[index]['answer3']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer4']['correct']}">${questions[index]['answer4']['answer']}</li>
+					<li class="answer-item" data-iscorrect="${questions[index]['answer5']['correct']}">${questions[index]['answer5']['answer']}</li>
 				</ol>`;
 			break;
 	}
 }
 
-//! Change group of questions and answers
+//! Add the form wraper for the last stage
+function lastStage() {
+	elQuestionSctn.classList.add('hidden__wraper');
+	elQuestionSctn.classList.remove('main__wraper');
+	goodbye.classList.remove('hidden__wraper');
+	elScoreFnl.textContent = String(score);
+}
 
-//TODO Appear first group
-//TODO If correct answer show green correct answer
-//TODO If wrong answer show red and borrow some time from the timer
-//TODO when correct appears button for next question
+/*=====  End of Section comment block  ======*/
 
-/* Features functions */
+/*=============================================
+=            EVENTS SECTION          =
+=============================================*/
 
-//! Set timer set
-let timer = setInterval(function () {
-	seconds--;
-	elTimer.textContent = `${minute}:${seconds}`;
+//!Click start game
+startBtn.addEventListener('click', startGame);
 
-	if (seconds < 10 && !(minute < 10)) {
-		elTimer.textContent = `${minute}:0${seconds}`;
-	} else if (seconds < 10 && minute < 10) {
-		elTimer.textContent = `0${minute}:0${seconds}`;
-	} else if (minute < 10 && seconds >= 10) {
-		elTimer.textContent = `0${minute}:${seconds}`;
+//!Delegation event to select correct answer
+elAnswersWraper.addEventListener('click', function (event) {
+	let correctAws = event.target.dataset.iscorrect;
+	if (correctAws === 'true') {
+		index++;
+		score += 20;
+		updateQstn(index);
+		index === questions.length ? lastStage() : false;
+	} else {
+		seconds -= 10;
+		seconds <= 0 ? quitGame() : false;
 	}
+});
 
-	if (minute === 0 && seconds === 0) {
-		seconds = 0;
-		minute = 0;
-		clearInterval(timer);
-		elTimer.textContent = `0${minute}:0${seconds}`;
-	} else if (seconds === 0) {
-		minute--;
-		seconds = 59;
-		/* elTimer.textContent = `0${minute}:${seconds}`; */
-	}
-}, 1000);
+/*=====  End of Section comment block  ======*/
 
-//! Display wrong or correct answer
+/*=============================================
+=            FORM GENERATOR           =
+=============================================*/
+
+//!Prevent default last button
+subBtn.addEventListener('click', function (event) {
+	//!Prevent Default
+	event.preventDefault();
+
+	//!User info object
+	let usersScoresInitials = {
+		userInit: elInicials.value.trim(),
+		userScore: elScoreFnl.textContent.trim(),
+	};
+	//!Convert the user object into a JSON file
+	localStorage.setItem('userInfo', JSON.stringify(usersScoresInitials));
+
+	console.log(elScoreFnl.textContent);
+});
+
+/*=====  End of Section comment block  ======*/
+
+/*=============================================
+=           MISC CODE           =
+=============================================*/
+
+//! Finish game and jump to last section
+function quitGame() {
+	clearInterval(timer);
+	return lastStage();
+}
+
+console.log(seconds);
+
+/*=====  End of Section comment block  ======*/
